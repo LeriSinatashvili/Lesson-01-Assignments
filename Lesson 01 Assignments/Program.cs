05b663b3d3ea5998eb0b4a743642603ad71da171
Leri Sinatashvili <sinatashvili.leri@outlook.com>
2024-09-27 22:51:35 +0400

    Add project files.

---
+++ Lesson 01 Assignments/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Put Number and i will tell you if its Positive, Negative or Zero");
+int RecievedNumber = int.Parse(Console.ReadLine());
+
+if (RecievedNumber > 0)
+{
+    Console.WriteLine("Your Number is Positive");
+}
+else if (RecievedNumber < 0)
+{
+    Console.WriteLine("Your Number is Negative");
+}
+else
+{
+    Console.WriteLine("Your Number is Zero");
+}
