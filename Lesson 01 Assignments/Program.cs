447b7d406764a7405fd0bec2e9f4fed1e894410d
Leri Sinatashvili <sinatashvili.leri@outlook.com>
2024-09-27 22:54:35 +0400

    Assignments 03

--- Lesson 01 Assignments/Program.cs
+++ Lesson 01 Assignments/Program.cs
@@ -1,11 +1,23 @@
-﻿Console.WriteLine("Put Number and i will tell you if its Even or Odd");
+﻿Console.WriteLine("Put Number and i will tell your Grade");
 int RecievedNumber = int.Parse(Console.ReadLine());
 
-if (RecievedNumber % 2 == 0)
+if (RecievedNumber >= 90)
 {
-    Console.WriteLine("Your Number is Even");
+    Console.WriteLine("You got A");
+}
+else if (RecievedNumber >= 80 && RecievedNumber <= 89)
+{
+    Console.WriteLine("You got B");
+}
+else if (RecievedNumber >= 70 && RecievedNumber <= 79)
+{
+    Console.WriteLine("You got C");
+}
+else if (RecievedNumber >= 60 && RecievedNumber <= 69)
+{
+    Console.WriteLine("You got D");
 }
 else
 {
-    Console.WriteLine("Your Number is Odd");
+    Console.WriteLine("Your got F");
 }
