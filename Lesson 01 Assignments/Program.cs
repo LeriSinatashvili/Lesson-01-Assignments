d0eddeb093a2d6b646938f71d6a86d95a1718fd3
Leri Sinatashvili <sinatashvili.leri@outlook.com>
2024-09-27 23:03:21 +0400

    Assignments 08

--- Lesson 01 Assignments/Program.cs
+++ Lesson 01 Assignments/Program.cs
@@ -1,31 +1,11 @@
-﻿Console.WriteLine("This is simple calculator");
-Console.WriteLine("Enter first number");
-int FirstNumber = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter operation you want");
-string Operation = Console.ReadLine();
-Console.WriteLine("Enter second number");
-int SecondNumber = int.Parse(Console.ReadLine());
+﻿Console.WriteLine("Tell me your Age and i will tell you if you are allowed to vote or not");
+int RecievedNumber = int.Parse(Console.ReadLine());
 
-if (Operation == "+")
+if (RecievedNumber >= 18)
 {
-    Console.WriteLine("Result is");
-    Console.WriteLine(FirstNumber + SecondNumber);
+    Console.WriteLine("You can Vote");
 }
-
-if (Operation == "-")
+else
 {
-    Console.WriteLine("Result is");
-    Console.WriteLine(FirstNumber - SecondNumber);
-}
-
-if (Operation == "*")
-{
-    Console.WriteLine("Result is");
-    Console.WriteLine(FirstNumber * SecondNumber);
-}
-
-if (Operation == "/")
-{
-    Console.WriteLine("Result is");
-    Console.WriteLine(FirstNumber / SecondNumber);
+    Console.WriteLine("Your Can Not Vote");
 }
